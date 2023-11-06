4c3eaae82989b74f200a147de94ca36c0b09f277
2111140081h <2111140081h@ed.fuk.kindai.ac.jp>
2023-11-06 09:46:06 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,12 @@
 Rails.application.routes.draw do
-  get 'tweet/index'
-  get 'tweet/new'
-  get 'tweet/create'
-  get 'tweet/destroy'
   get 'top/main'
   get 'top/login'
+  post 'top/login'
   get 'top/logout'
-  get 'top/failed'
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  get 'users/destroy'
-  get 'tweets/index'
-  get 'tweets/new'
-  get 'tweets/create'
-  get 'tweets/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :users
   resources :tweet
-  resources :top
   resources :likes, only: [:create, :destroy]
-  post 'users/create'
+
   root 'users#index'
-  get 'top/main'
-  post 'top/login'
-  post 'top/logout'
-  get 'top/logout'
-  post 'tweet/create'
 end
