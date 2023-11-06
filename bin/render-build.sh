742487c9ffc12aa8bbf49e6d4092297a3654e23d
2111140081h <2111140081h@ed.fuk.kindai.ac.jp>
2023-11-06 09:58:12 +0000

    a

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,5 @@
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:reset
+RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:reset
 
